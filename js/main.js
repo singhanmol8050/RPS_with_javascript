55e3266043923f2ac47dc93b2a58d23d98b380e8
Dhaliwal <a_dhaliwal110583@ats-hr420-dm24.fc.ca>
2019-11-05 14:38:23 -0500

    updated

---
+++ js/main.js
@@ -0,0 +1,32 @@
+(() => {
+	console.log('fired!');
+
+	let player_lives = 5,
+	    player = document.querySelector("#weapon"),
+	    computer,
+	    trigger = document.querySelector("#play"),
+	    status = document.querySelector(".winlose"),
+	    computer_lives = 5;
+
+	const choices = ["rock", "paper", "scissors"];
+	
+	function playGame() {
+		console.log('clicked the button');
+
+		computer = choices[Math.floor(Math.random() * choices.length)];
+		
+		let player_choice = player.value;
+
+		console.log("computer:", computer, "player:", player_choice);
+
+		// this is a comment. check for a tie
+		if(computer == player_choice) {
+			// tie - no one wins
+			status.textContent = "Tie! You live to shoot another day!";
+		} else {
+			status.textContent = "";
+		}
+	}
+
+	trigger.addEventListener("click", playGame);    
+})();
